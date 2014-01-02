548e667828278cf7c2a6751c2244b749d576958c
Bruno Sanches <brunopsanches@gmail.com>
2014-01-02 05:57:49 -0800

    Listen to ALL the ports
    * Add 'https' module to listen for SSL (https) connections
    * Listen on port 8080
    * Separate response function handler to one function so it can be reused

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var http = require('http');
 var send = require('send');
 
-var server = http.createServer(function(req, res) {
-  // console.log('connected!');
+var fs = require('fs');
+var https = require('https');
+
+http.createServer(servePage).listen(80);
+http.createServer(servePage).listen(8080);
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+https.createServer(options, servePage).listen(443);
+
+function servePage(req, res) {
+  console.log('connected!');
 
   send(req, req.url).root(__dirname).pipe(res);
-});
-server.listen(80);
+}
